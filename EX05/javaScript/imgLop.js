5deaab066a1a0e9b0461b6477ef6630538f533bf
Henrique Lira <henrique.2019210942@unicap.br>
2022-04-30 21:25:55 -0300

    ajustesf

--- EX05/javaScript/imgLop.js
+++ EX05/javaScript/imgLop.js
@@ -1,29 +1,23 @@
 let time = 5000,
-    currentImageIndex = 0,
-    images = document
-                .querySelectorAll("#slider img")
-    max = images.length;
+  currentImageIndex = 0,
+  images = document.querySelectorAll("#slider img");
+max = images.length;
 
 function nextImage() {
+  images[currentImageIndex].classList.remove("selected");
 
-    images[currentImageIndex]
-        .classList.remove("selected")
+  currentImageIndex++;
 
-    currentImageIndex++
+  if (currentImageIndex >= max) currentImageIndex = 0;
 
-    if(currentImageIndex >= max)
-        currentImageIndex = 0
-
-    images[currentImageIndex]
-        .classList.add("selected")
+  images[currentImageIndex].classList.add("selected");
 }
-
 
 function start() {
-    setInterval(() => {
-        // troca de image
-        nextImage()
-    }, time)
+  setInterval(() => {
+    // troca de image
+    nextImage();
+  }, time);
 }
 
-window.addEventListener("load", start)
+window.addEventListener("load", start);
